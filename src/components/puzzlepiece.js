fa132f3834c230f54c303344972d77961e7f5a19
houzairkoussa <houzairmk@icloud.com>
2021-12-11 09:40:05 +0400

    added randomness to tile generation

--- src/components/puzzlepiece.js
+++ src/components/puzzlepiece.js
@@ -2,14 +2,24 @@ import React from "react";
 
 // renders individual puzzle pieces
 function PuzzlePiece () {
-  const tileArray = [];
-  for (let index = 0; index < 15; index++) tileArray.push(index);
+  const tiles = [];
+  for (let index = 0; index < 16; index++) {
+    let random = Math.round(Math.random() * 15);
+    while (tiles.includes(random)) (random = Math.round(Math.random() * index));
+    tiles.push(random);
+  }
+
+  const handleClass = (index) => {
+    return (index !== 0) ? "puzzle-piece" : "";
+  }
 
   return(
     <>
-      {tileArray.map( (index) =>
-        <div className="puzzle-piece" key={index}>
-          {index + 1}
+      {tiles.map( (index) =>
+        <div
+          className={handleClass(index)}
+          key={index}>
+            {(index !== 0) ? index : ""}
         </div>
       )}
     </>
