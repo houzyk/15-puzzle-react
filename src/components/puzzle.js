465abaa3067af861f2c0d4392b1a71f8f0eb5b1c
houzairkoussa <houzairmk@icloud.com>
2021-12-11 21:07:02 +0400

    puzzle piece handles click

--- src/components/puzzle.js
+++ src/components/puzzle.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import PuzzlePiece from "./puzzlepiece";
 
 // renders puzzle board
-function Puzzle () {
-  return(
-    <div className="puzzle">
-      <PuzzlePiece />
-    </div>
-  );
+class Puzzle extends Component {
+  constructor (props) {
+    super(props);
+    const tiles = [];
+    for (let index = 0; index < 16; index++) {
+      let random = Math.round(Math.random() * 15);
+      while (tiles.includes(random)) (random = Math.round(Math.random() * index));
+      tiles.push(random);
+    }
+    this.state = { tiles: tiles }
+  }
+
+  gameMove () {
+
+  }
+
+  render () {
+    return(
+      <div className="puzzle">
+        <PuzzlePiece tiles={this.state.tiles} gameMove={this.gameMove}/>
+      </div>
+    );
+  }
 }
 
 export default Puzzle;
--- src/components/puzzlepiece.js
+++ src/components/puzzlepiece.js
@@ -2,34 +2,19 @@ import React, { Component } from "react";
 
 // renders individual puzzle pieces
 class PuzzlePiece extends Component {
-  constructor (props) {
-    super(props);
-    const tiles = [];
-    for (let index = 0; index < 16; index++) {
-      let random = Math.round(Math.random() * 15);
-      while (tiles.includes(random)) (random = Math.round(Math.random() * index));
-      tiles.push(random);
-    }
-    this.state = { tiles: tiles }
-  }
-
   handleClass (index) {
     return (index !== 0) ? "puzzle-piece" : "";
-  }
-
-  handleClick (e) {
-
   }
 
   render () {
     return(
       <>
-        {this.state.tiles.map( (index) =>
+        {this.props.tiles.map( (index) =>
           <div
             className={this.handleClass(index)}
             key={index}
             data-number={index}
-            onClick={this.handleClick}>
+            onClick={this.props.gameMove}>
               {(index !== 0) ? index : ""}
           </div>
         )}
